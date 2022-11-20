84cab85626dde85829c6129e140cdafa621fbc48
Alexandre Lozano Vilanova <alexandre.lozano@gmail.com>
2022-11-20 08:45:34 +0100

    Add status bar

--- jpgvbnet/FMain.Designer.vb
+++ jpgvbnet/FMain.Designer.vb
@@ -33,10 +33,15 @@ Partial Class FMain
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblJPGFilename = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblJPGSize = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblJPGMilliseconds = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.pgbJPGDecode = New System.Windows.Forms.ToolStripProgressBar()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'PictureBox1
@@ -99,25 +104,47 @@ Partial Class FMain
         Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
         Me.AboutToolStripMenuItem.Text = "About"
         '
-        'ProgressBar1
-        '
-        Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.ProgressBar1.Location = New System.Drawing.Point(0, 446)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(476, 23)
-        Me.ProgressBar1.TabIndex = 2
-        Me.ProgressBar1.Visible = False
-        '
         'Timer1
         '
         Me.Timer1.Interval = 500
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.pgbJPGDecode, Me.lblJPGFilename, Me.lblJPGSize, Me.lblJPGMilliseconds})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 447)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(476, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblJPGFilename
+        '
+        Me.lblJPGFilename.Name = "lblJPGFilename"
+        Me.lblJPGFilename.Size = New System.Drawing.Size(0, 17)
+        '
+        'lblJPGSize
+        '
+        Me.lblJPGSize.Name = "lblJPGSize"
+        Me.lblJPGSize.Size = New System.Drawing.Size(0, 17)
+        '
+        'lblJPGMilliseconds
+        '
+        Me.lblJPGMilliseconds.Name = "lblJPGMilliseconds"
+        Me.lblJPGMilliseconds.Size = New System.Drawing.Size(0, 17)
+        '
+        'pgbJPGDecode
+        '
+        Me.pgbJPGDecode.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.pgbJPGDecode.Name = "pgbJPGDecode"
+        Me.pgbJPGDecode.Size = New System.Drawing.Size(100, 16)
+        Me.pgbJPGDecode.Style = System.Windows.Forms.ProgressBarStyle.Continuous
         '
         'FMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(476, 469)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -128,6 +155,8 @@ Partial Class FMain
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -142,6 +171,10 @@ Partial Class FMain
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
-    Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblJPGFilename As ToolStripStatusLabel
+    Friend WithEvents lblJPGSize As ToolStripStatusLabel
+    Friend WithEvents lblJPGMilliseconds As ToolStripStatusLabel
+    Friend WithEvents pgbJPGDecode As ToolStripProgressBar
 End Class
--- jpgvbnet/FMain.vb
+++ jpgvbnet/FMain.vb
@@ -12,17 +12,23 @@ Public Class FMain
 
             Cursor = Cursors.WaitCursor
 
+            lblJPGFilename.Text = My.Computer.FileSystem.GetName(OpenFileDialog1.FileName)
+            lblJPGSize.Text = GetFileSize(OpenFileDialog1.FileName)
+            lblJPGMilliseconds.Text = ""
+
             PictureBox1.Image = Nothing
             PictureBox1.Refresh()
 
             MenuStrip1.Enabled = False
-            ProgressBar1.Visible = True
             Timer1.Enabled = True
 
+            Dim stopWatch = New Stopwatch()
             jpg = New jpg
             Dim bmp As Bitmap = Nothing
             t = New Thread(Sub()
+                               stopWatch.Start()
                                bmp = jpg.DoJPG(OpenFileDialog1.FileName)
+                               stopWatch.Stop()
                            End Sub)
             t.Start()
 
@@ -30,11 +36,14 @@ Public Class FMain
                 Application.DoEvents()
             End While
 
+            lblJPGMilliseconds.Text = stopWatch.ElapsedMilliseconds & " ms"
+
             PictureBox1.Image = bmp
             PictureBox1.Refresh()
 
+            pgbJPGDecode.Value = pgbJPGDecode.Maximum
+
             MenuStrip1.Enabled = True
-            ProgressBar1.Visible = False
             Timer1.Enabled = False
             Cursor = Cursors.Default
 
@@ -62,16 +71,45 @@ Public Class FMain
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
 
-        t.Suspend()
-
-        ProgressBar1.Maximum = jpg.flen
-        ProgressBar1.Value = jpg.findex
-        ProgressBar1.Refresh()
-
-        PictureBox1.Image = jpg.bmp.Clone
-
-        t.Resume()
+        pgbJPGDecode.Maximum = jpg.flen
+        pgbJPGDecode.Value = jpg.findex
 
     End Sub
+
+    Public Function GetFileSize(ByVal TheFile As String) As String
+
+        Dim DoubleBytes As Double
+
+        If TheFile.Length = 0 Then Return ""
+        If Not System.IO.File.Exists(TheFile) Then Return ""
+
+        Dim TheSize As ULong = My.Computer.FileSystem.GetFileInfo(TheFile).Length
+        Dim SizeType As String = ""
+
+        Try
+            Select Case TheSize
+                Case Is >= 1099511627776
+                    DoubleBytes = CDbl(TheSize / 1099511627776) 'TB
+                    Return FormatNumber(DoubleBytes, 2) & " TB"
+                Case 1073741824 To 1099511627775
+                    DoubleBytes = CDbl(TheSize / 1073741824) 'GB
+                    Return FormatNumber(DoubleBytes, 2) & " GB"
+                Case 1048576 To 1073741823
+                    DoubleBytes = CDbl(TheSize / 1048576) 'MB
+                    Return FormatNumber(DoubleBytes, 2) & " MB"
+                Case 1024 To 1048575
+                    DoubleBytes = CDbl(TheSize / 1024) 'KB
+                    Return FormatNumber(DoubleBytes, 2) & " KB"
+                Case 0 To 1023
+                    DoubleBytes = TheSize ' bytes
+                    Return FormatNumber(DoubleBytes, 2) & " bytes"
+                Case Else
+                    Return ""
+            End Select
+        Catch
+            Return ""
+        End Try
+
+    End Function
 
 End Class
